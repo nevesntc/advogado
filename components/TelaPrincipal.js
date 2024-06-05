780fe626eca66465cf06c89d2993ba9f7dd2898e
Bruno Neves <131283126+nevesntc@users.noreply.github.com>
2024-06-05 13:57:19 -0300

    Update TelaPrincipal.js

--- components/TelaPrincipal.js
+++ components/TelaPrincipal.js
@@ -6,7 +6,7 @@ const TelaPrincipal = () => {
   return (
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <div style={{ backgroundImage: 'url(/background.jpg)', height: '300px', backgroundSize: 'cover' }}>
-        <h1>Bem vindo ao MeuJurídico</h1>
+        <h1>Bem vindo ao AppJurídico</h1>
       </div>
       <Link to="/processos">Ver Processos</Link>
       <br />
